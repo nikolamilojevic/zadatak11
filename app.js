415e94e02be50e8f42510e9e26c84a32f53f0738
Nikola <nikola.milojevic@vivifyideas.com>
2018-11-09 09:41:40 +0100

    for review

---
+++ app.js
@@ -0,0 +1,16 @@
+var a = 'var from app.js';
+let b = 'let from app.js';
+
+var myVars = function() {
+	var e = 'var from inside anonymous function';
+	console.log(a);
+	console.log(b);
+	console.log(c);
+	// console.log(d);
+	console.log(e);
+	console.log(f);
+	// console.log(g);
+}
+
+myVars();
+
---
+++ under.js
@@ -0,0 +1,1 @@
+var g = 'from under app.js';
